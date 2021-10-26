b158811ce2b946fae030125ec5639117ddff4732
Antonio Sucre <antonio.sucre@indikatore.com>
2021-10-26 04:46:24 -0300

    add-form-states

--- src/upload.js
+++ src/upload.js
@@ -6,36 +6,57 @@ import {
 import axios from 'axios';
 
 function Upload() {
+  const [imagem, setImagem] = useState();
+  const [desabilitarBotao, setDesabilitarBotao] = useState(true);
+  const [urlImagem, setUrlImagem] = useState();
+  const [exibirImagem, setExibirImagem] = useState(false);
+  const [exibirModal, setExibirModal] = useState(false);
+  const [exibirProcessando, setExibirProcessando] = useState(false);
+
+  function handleUpload(event) {
+
+  }
+
+  function handleImagem(event) {
+    setImagem(event.target.files[0]);
+    setDesabilitarBotao(false);
+  }
+
+  function handleFecharModal() {
+    setExibirModal(false);
+  }
+
   return (
     <div>
       <h3 className="text-center">Upload de imagens</h3>
       <div class="container-fluid py-5">
-        <Form noValidate>
+        <Form onSubmit={handleUpload} noValidate>
           <Form.Group>
             <Form.Label>Selecione a imagem (PNG ou JPEG)</Form.Label>
-            <Form.Control type="file" accept="image/png, image/jpeg" />
+            <Form.Control type="file" accept="image/png, image/jpeg" onChange={handleImagem} />
           </Form.Group>
           <Form.Group className="text-center">
             <Button
               variant="success"
-              type="submit">
+              type="submit"
+              disabled={desabilitarBotao}>
               <span>Fazer upload</span>
             </Button>
           </Form.Group>
-          <Form.Group>
+          <Form.Group className={exibirProcessando ? 'text-center' : 'hidden'}>
             <Spinner animation="border" />
           </Form.Group>
         </Form>
       </div>
-      <div>
+      <div className={exibirImagem ? 'text-center' : 'hidden'}>
         <hr />
-        <a href="" target="_blank">
-          <Image src={""} thumbnail />
+        <a href={urlImagem} target="_blank">
+          <Image src={urlImagem} thumbnail />
           <br />
-          {/* url em texto */}
+          {urlImagem}
         </a>
       </div>
-      <Modal show={false}>
+      <Modal show={exibirModal} onHide={handleFecharModal}>
         <Modal.Header closeButton>
           <Modal.Title>Erro ao fazer o upload da imagem</Modal.Title>
         </Modal.Header>
@@ -43,7 +64,7 @@ function Upload() {
           Nao foi possivel fazer o upload da imagem, tente novamente em instantes
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="warning">Fechar</Button>
+          <Button variant="warning" onClick={handleFecharModal}>Fechar</Button>
         </Modal.Footer>
       </Modal>
     </div>
